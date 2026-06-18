e0f305ad22874217eeb3fff2fd65cfe2cd5090d7
mahmudde <mahmud71113@gmail.com>
2026-06-18 23:54:13 +0600

    Remove InitialScroll to start page on Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,10 @@ import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
-import InitialScroll from "@/components/InitialScroll";
 
 export default function Home() {
   return (
     <>
-      <InitialScroll />
       <Hero />
       <About />
       <Skills />
